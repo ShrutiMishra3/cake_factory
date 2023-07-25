63f2ff5388a3cb528250b148fcedbc74b042db28
Prasant Kumar Mahato <prasantmahato33@gmail.com>
2023-07-25 22:09:51 +0530

    ADD(): Session Model

---
+++ backend/models/Session.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const sessionSchema = new mongoose.Schema({
+  sessionId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Session = mongoose.model('Session', sessionSchema);
+
+module.exports = Session;
