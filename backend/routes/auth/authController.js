60664d0ceb20e238b4918ab511ef53c58aa4a747
Prasant Kumar Mahato <prasantmahato33@gmail.com>
2023-09-23 15:05:44 +0530

    Added success or failure status to response

--- backend/routes/auth/authController.js
+++ backend/routes/auth/authController.js
@@ -8,7 +8,7 @@ module.exports.Signup = async (req, res, next) => {
     const { email, password, name, address } = req.body;
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.json({ message: "User already exists" }).status(301);
+      return res.json({ message: "User already exists", success: false }).status(301);
     }
     const user = await User.create({ email, password, name, address });
     const token = createSecretToken(user._id);
@@ -18,7 +18,7 @@ module.exports.Signup = async (req, res, next) => {
     });
     res
       .status(201)
-      .json({ message: "User signed in successfully", success: true, user });
+      .json({ message: "User registered successfully", success: true, user });
     next();
   } catch (error) {
     console.error(error);
@@ -26,25 +26,26 @@ module.exports.Signup = async (req, res, next) => {
 };
 
 module.exports.Login = async (req, res, next) => {
+    const isSuccess = false;
     try {
       const { email, password } = req.body;
       if(!email || !password ){
-        return res.json({message:'All fields are required'})
+        return res.json({message:'All fields are required', success: isSuccess})
       }
       const user = await User.findOne({ email });
       if(!user){
-        return res.json({message:'Incorrect password or email' }) 
+        return res.json({message:'Incorrect password or email', success: isSuccess }) 
       }
       const auth = await bcrypt.compare(password,user.password)
       if (!auth) {
-        return res.json({message:'Incorrect password or email' }) 
+        return res.json({message:'Incorrect password or email', success: isSuccess }) 
       }
        const token = createSecretToken(user._id);
        res.cookie("token", token, {
          withCredentials: true,
          httpOnly: false,
        });
-       res.status(201).json({ message: "User logged in successfully", success: true });
+       res.status(201).json({ message: "User logged in successfully", success: !isSuccess });
        next()
     } catch (error) {
       console.error(error);
